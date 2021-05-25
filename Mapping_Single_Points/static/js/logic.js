921901193792ade2ca22fd375ab4620dbf76a378
opeyemi ojekunle <76888695+opeoje91@users.noreply.github.com>
2021-05-25 13:47:52 -0500

    orange marker

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -9,7 +9,9 @@ let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
 //  Add a circle to the map for Los Angeles, California.
 L.circle([34.0522, -118.2437], {
-    radius: 100
+    radius: 300,
+    color: "black",
+    fillColor: "#ffffa1"
  }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
