32bb72ab77f0ba4803e6257f1229a2b230cab328
opeyemi ojekunle <76888695+opeoje91@users.noreply.github.com>
2021-05-26 15:36:27 -0500

    lo

--- Mapping_GeoJSON_Polygons/static/js/logic.js
+++ Mapping_GeoJSON_Polygons/static/js/logic.js
@@ -7,7 +7,11 @@ console.log("working");
 // Create the map object with center and zoom level.
 // let map = L.map('mapid').setView([30, 30], 2);
 
+<<<<<<< Updated upstream
 let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+=======
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+>>>>>>> Stashed changes
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -16,7 +20,11 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // streets.addTo(map);
 
 // We create the dark view tile layer that will be an option for our map.
+<<<<<<< Updated upstream
 let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+=======
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+>>>>>>> Stashed changes
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -24,19 +32,31 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
+<<<<<<< Updated upstream
   Light: light,
   Dark: dark
+=======
+  "Streets": streets,
+  "Satellite Streets": satelliteStreets
+>>>>>>> Stashed changes
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
+<<<<<<< Updated upstream
   center: [44.0, -80.0],
   zoom: 2,
   layers: [light]
+=======
+  center: [43.7, -79.3],
+  zoom: 11,
+  layers: [satelliteStreets]
+>>>>>>> Stashed changes
 });
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
+<<<<<<< Updated upstream
 // Accessing the airport GeoJSON URL
 let torontoData = "https://raw.githubusercontent.com/opeoje91/Mapping_Earthquake/main/torontoRoutes.json";
 
@@ -55,4 +75,19 @@ L.geoJson(data,  {
     layer.bindPopup("<h2>Airline: " + feature.properties.airline + "</h2><hr><h3>Destination: "+ feature.properties.dst+ "</h3>");
   }
   }).addTo(map)
+=======
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let torontoHoods = "https://raw.githubusercontent.com/opeoje91/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+// // Create a style for the lines.
+// let myStyle = {
+//   color: "#ffffa1",
+//   weight: 2
+// };
+
+d3.json(torontoHoods).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJson(data).addTo(map)
+>>>>>>> Stashed changes
 });
